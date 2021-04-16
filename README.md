dc913e2bafd54b62249020097d6b0ae13cc6ecdb
Sharon Templin <sharon@hollygo.es>
2021-04-16 15:00:42 -0500

    Data from data.js is in table in index.html.  Added createTable fxn in app.js

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ javascript homework - javascript and DOM manipulation
 ## development notes
 - starting with UFO-1.  
 - Using the UFO dataset, write code that appends a table to the web page and then adds new rows of data for each UFO sighting.
+- Note to self:  creating an HTML table by hand is possible, but way too much work, too error prone.  With some digging, found a much cleaner approach as reflected in the function createTable.  All of the day is showing up on the web page.
+- 
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,21 +1,29 @@
 // from data.js
 var tableData = data;
+//console.log(data);
 
-// for reference... delete me
-// var data = [{
-//     datetime: "1/1/2010",
-//     city: "benton",
-//     state: "ar",
-//     country: "us",
-//     shape: "circle",
-//     durationMinutes: "5 mins.",
-//     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
-//   }
-
-
-// YOUR CODE HERE!
 
 console.log("app.js is loaded");
+var table = document.querySelector("table");
+// var table_header = table.querySelector("thead");
+// var table_body = table.querySelector("tbody");
+
+console.log(data.length);
+
+function createTable(table,tableData) {
+    for (var element of tableData){
+        var row = table.insertRow();
+        for (key in element){
+            var cell = row.insertCell();
+            var text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+    console.log(table);
+    return table;
+}
+
+table = createTable(table,tableData);
 
 // use data from data.js to populate webpage table
 
