af99216ce34f6e4ba24a6ced34ec89467e120849
Sharon Templin <sharon@hollygo.es>
2021-04-19 19:43:27 -0500

    Updated app.js after re-watching class video

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,26 +3,75 @@ var tableData = data;
 //console.log(data);
 
 
-console.log("app.js is loaded");
-var table = document.querySelector("table");
-// var table_header = table.querySelector("thead");
-// var table_body = table.querySelector("tbody");
 
-console.log(data.length);
 
-function createTable(table,tableData) {
-    for (var element of tableData){
-        var row = table.insertRow();
-        for (key in element){
-            var cell = row.insertCell();
-            var text = document.createTextNode(element[key]);
-            cell.appendChild(text);
-        }
-    }
+function createTable(table,newTableData) {
+    console.log('createTable');
+    
+    console.log(table.rows.length);
+    
+    var tbody = d3.select("tbody");
+    
+    newTableData.forEach(function(ufo_report){
+        var row =  tbody.append("tr");
+        
+        Object.entries(ufo_report).forEach(function([key, value]) {
+            var cell = row.append("td");       
+            cell.text(value);
+        });
+    });
     console.log(table);
-    return table;
+    return;
 }
 
+function updateTable(selectedData){
+    var rows = d3.selectAll('tbody tr')
+        .data(selectedData, function (d) { return d;});
+    
+    rows.enter()
+        .append('tr')
+        .selectAll('td')
+        .data(function (d) {return [d.key, d.value];})
+        .enter()
+        .append('td')
+        .text(function(d) {  return d; });
+        
+    rows.exit().remove();
+    
+    var cells = rows.selectAll('td')
+        .data(function (d) { return [d.key, d.value];})
+        .text(document.createTextNode(element[key]));
+    
+    cells.enter()
+        .append('td')
+        .text(function(d) { return d; });
+    
+    cells.exit().remove();
+
+    var table = document.querySelector("tbody");
+    
+    createTable(table, selectedData);
+    // var table = d3.select("tbody")
+    //     .selectAll("tr")
+    //     .data(selectedData)
+    //     .selectAll("td")
+    //     .data(function(row) {
+    //         return columns.map(function(column) {
+    //             return {
+    //                 column: column,
+    //                 value: row[column]
+    //             };
+    //         });
+    //     })
+    //     .text(function(d) {  return d.value; });
+
+    
+    // rows.enter().append("tr")
+    //     .text(function(d) { return d; });
+    
+   
+
+}
 
 var button = d3.select("#filter-btn");
 var form = d3.select("form");
@@ -32,7 +81,9 @@ button.on("click", runEnter);
 form.on("submit", runEnter);
 
 function runEnter() {
+    // Prevent the page from refreshing
     d3.event.preventDefault();
+
     console.log("button clicked or something!!")
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
@@ -42,20 +93,25 @@ function runEnter() {
     console.log(selectedData);
     // use input value to select data 
     
-
+    //var ufo_table = document.querySelector("#ufo-table");
+    //createTable(table,selectedData);
+    updateTable(table, selectedData);
     // if no data found for entered date... 
    
 }
 
 
-function selectByDate(date) {
-    console.log('selectByDate');
-    console.log(data.datetime == date);
-    return data.datetime == date;
-}
 
 
-table = createTable(table,tableData);
+console.log("app.js is loaded");
+var table = document.querySelector("tbody");
+//var table = document.getElementById('ufo-table');
+// var table_header = table.querySelector("thead");
+// var table_body = table.querySelector("tbody");
+
+console.log(data.length);
+
+createTable(table,tableData);
 
 // use data from data.js to populate webpage table
 
